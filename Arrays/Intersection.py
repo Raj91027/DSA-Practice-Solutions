e5a3492e17244132b89ddab8f8b2eae86faad671
Raj Kumar Yadav <rajkumar08bgh@gmail.com>
2024-02-25 14:51:22 +0530

    Day2 commit

---
+++ Arrays/Intersection.py
@@ -0,0 +1,34 @@
+# Intersection of two Arrays..
+
+
+#User function Template for python3
+
+#Function to return the count of the number of elements in
+#the intersection of two arrays.
+class Solution:
+    def NumberofElementsInIntersection(self,a, b, n, m):
+        ar = set(a)
+        br = set(b)
+        inter = ar.intersection(br)
+        return len(inter)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob = Solution()
+        
+        print(ob.NumberofElementsInIntersection(a,b,n,m))
+    
+    
+                
+# } Driver Code Ends
