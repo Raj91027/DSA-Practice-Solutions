990110de743c9305f5182a0e40e824365d4884ac
Raj Kumar Yadav <rajkumar08bgh@gmail.com>
2024-03-01 23:24:23 +0530

    Merge without extra space

---
+++ Arrays/Merge sorted array without extra space.cpp
@@ -0,0 +1,163 @@
+/*
+    Merge Without Extra Space
+    Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. 
+    Merge them in sorted order without using any extra space. Modify arr1 so that it contains 
+    the first N elements and modify arr2 so that it contains the last M elements.
+
+Example 1:
+
+Input: 
+n = 4, arr1[] = [1 3 5 7] 
+m = 5, arr2[] = [0 2 6 8 9]
+Output: 
+arr1[] = [0 1 2 3]
+arr2[] = [5 6 7 8 9]
+Explanation:
+After merging the two 
+non-decreasing arrays, we get, 
+0 1 2 3 5 6 7 8 9.
+
+
+Link:https://www.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/1
+
+*/
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            int i = 0, j = 0, k = n - 1;
+ 
+            while (i <= k && j < m) {
+                if (arr1[i] < arr2[j])
+                    i++;
+                else {
+                    swap(arr2[j++], arr1[k--]);
+                }
+            }
+         
+           
+            sort(arr1, arr1 + n);
+         
+           
+            sort(arr2, arr2 + m);
+                
+        } 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+
+// } Driver Code Ends
+
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int m = Integer.parseInt(inputLine[1]);
+		    long arr1[] = new long[n];
+		    long arr2[] = new long[m];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr1[i] = Long.parseLong(inputLine[i]);
+		    }
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<m; i++){
+		        arr2[i] = Long.parseLong(inputLine[i]);
+		    }
+		    Solution ob = new Solution();
+		    ob.merge(arr1, arr2, n, m);
+		    
+		    StringBuffer str = new StringBuffer();
+		    for(int i=0; i<n; i++){
+		        str.append(arr1[i]+" ");
+		    }
+		    for(int i=0; i<m; i++){
+		        str.append(arr2[i]+" ");
+		    }
+		    System.out.println(str);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to merge the arrays.
+    public static void merge(long arr1[], long arr2[], int n, int m) 
+    {
+        for(int i=0; i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(arr1[j]>arr2[i])
+                {
+                    long temp = arr1[j];
+                    arr1[j] = arr2[i];
+                    arr2[i] = temp;
+                }
+            }
+        }
+        sort(arr2, arr2 + m);
+    }
+}
