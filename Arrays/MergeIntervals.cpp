4912e16ac8007716b7a9cc4c34f5e956714f840a
Raj Kumar Yadav <rajkumar08bgh@gmail.com>
2024-03-02 23:50:12 +0530

    MergeIntervals

---
+++ Arrays/MergeIntervals.cpp
@@ -0,0 +1,79 @@
+/*Given a collection of Intervals, the task is to merge all of the overlapping Intervals.
+
+Example 1:
+
+Input:
+Intervals = {{1,3},{2,4},{6,8},{9,10}}
+Output: {{1, 4}, {6, 8}, {9, 10}}
+Explanation: Given intervals: [1,3],[2,4]
+[6,8],[9,10], we have only two overlapping
+intervals here,[1,3] and [2,4]. Therefore
+we will merge these two and return [1,4],
+[6,8], [9,10].
+
+Expected Time Complexity: O(N*Log(N)).
+Expected Auxiliary Space: O(Log(N)) or O(N).
+
+Constraints:
+1 ≤ N ≤ 100
+0 ≤ x ≤ y ≤ 1000
+*/
+
+
+
+
+
+
+// C++ program to merge overlapping Intervals in
+// O(n Log n) time and O(1) extra space.
+#include <bits/stdc++.h>
+using namespace std;
+
+// An Interval
+struct Interval {
+	int s, e;
+};
+
+// Function used in sort
+bool mycomp(Interval a, Interval b) { return a.s < b.s; }
+
+void mergeIntervals(Interval arr[], int n)
+{
+	// Sort Intervals in increasing order of
+	// start time
+	sort(arr, arr + n, mycomp);
+
+	int index = 0; // Stores index of last element
+	// in output array (modified arr[])
+
+	// Traverse all input Intervals
+	for (int i = 1; i < n; i++) {
+		// If this is not first Interval and overlaps
+		// with the previous one
+		if (arr[index].e >= arr[i].s) {
+			// Merge previous and current Intervals
+			arr[index].e = max(arr[index].e, arr[i].e);
+		}
+		else {
+			index++;
+			arr[index] = arr[i];
+		}
+	}
+
+	// Now arr[0..index-1] stores the merged Intervals
+	cout << "\n The Merged Intervals are: ";
+	for (int i = 0; i <= index; i++)
+		cout << "[" << arr[i].s << ", " << arr[i].e << "] ";
+}
+
+// Driver program
+int main()
+{
+	Interval arr[]
+		= { { 6, 8 }, { 1, 9 }, { 2, 4 }, { 4, 7 } };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	mergeIntervals(arr, n);
+	return 0;
+}
+
+// This code is contributed by Aditya Kumar (adityakumar129)
