e5a61bf0783e3d102b759a861ccfe85858543c30
Raj Kumar Yadav <rajkumar08bgh@gmail.com>
2024-03-06 22:56:55 +0530

    Update validParenthesis

--- Arrays/ValidParenthesis.cpp
+++ Arrays/ValidParenthesis.cpp
@@ -24,11 +24,11 @@ Otherwise, return false, because some opening brackets have not been matched wit
 
 Complexity
 Time complexity:
-The time complexity of the solution is O(n)O(n)O(n), where n is the length of the input string. 
+The time complexity of the solution is O(n), where n is the length of the input string. 
 This is because we traverse the string once and perform constant time operations for each character.
 
 Space complexity:
-The space complexity of the solution is O(n)O(n)O(n), where n is the length of the input string. 
+The space complexity of the solution is O(n), where n is the length of the input string. 
 This is because the worst-case scenario is when all opening brackets are present in the string and the stack will have to store them all.
 */
 
