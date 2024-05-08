87beee801a2fc61f9b2a9d887b6a61f9d51c51a6
Raj Kumar Yadav <rajkumar08bgh@gmail.com>
2024-05-08 23:37:10 +0530

    FAANG

---
+++ Queues/QueueReversal.cpp
@@ -0,0 +1,99 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Approach-1: Using Stack
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//function Template for C++
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        stack<int> s;
+        
+        while(!q.empty()){
+            s.push(q.front());
+            q.pop();
+        }
+        
+        while(!s.empty()){
+            q.push(s.top());
+            s.pop();
+        }
+        
+        return q;
+    }
+};
+
+//Approach-2: Using recursion
+class Solution
+{
+    private:
+    void reverse(queue<int> &q){
+        if(q.empty()){
+            return;
+        }
+        
+        int ele = q.front();
+        q.pop();
+        reverse(q);
+        
+        q.push(ele);
+    }
+    
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        reverse(q);
+        return q;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    Solution ob;
+    queue<int> a=ob.rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
