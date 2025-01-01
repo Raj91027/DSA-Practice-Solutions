d039f21e824ef39948c46ce6d6ba5ee9e2923200
Raj Kumar Yadav <rajkumar08bgh@gmail.com>
2025-01-01 20:53:18 +0530

    Prime Numbers

---
+++ CodingQuestions/primeNo.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    int flag=0;
+
+    cout<<"Enter number to check:"<<endl;
+
+    cin>>n;
+
+    for(int i=2; i*i <= n; i++){
+        if(n %i==0){
+            cout<<"It is not prime"<<endl;
+            flag=1;
+            break;
+        }
+      
+    }
+    if(flag==0)
+        cout<<"it is a prime number"<<endl;
+}
